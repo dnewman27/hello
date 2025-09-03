9c8d69d42259e1ebde3f6021eef839a49cbaf30c
rtysdal2 <rtysdal2@unl.edu>
2025-09-03 04:33:48 +0000

    Tue, Sep 2, 2025, 11:33 PM -05:00

--- hello.c
+++ hello.c
@@ -10,6 +10,7 @@ int main() {
 
     printf("Dalton Newman\n");
     printf("My major is computer engineering\n");
-
+    printf("Rachel Tysdal\n");
+    printf("My major is Electrical engineering\n");
     return 0;
 }
